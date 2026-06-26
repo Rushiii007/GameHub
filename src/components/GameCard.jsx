3617dfa0425bb9e6c013b816f08c280efd54c8c5
Rushiii007 <2024mcb1301@iitrpr.ac.in>
2026-06-26 19:19:46 +0530

    Update GameCard.jsx

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,10 +1,11 @@
 import { getScore } from "../utils/scoreStorage";
 
-export default function GameCard({ title, description, tag, scoreKey, onClick }) {
+export default function GameCard({ icon, title, description, tag, scoreKey, onClick }) {
   const score = scoreKey ? getScore(scoreKey) : 0;
 
   return (
     <div className="game-card" onClick={onClick}>
+      <div className="game-icon">{icon}</div>
       <h3>{title}</h3>
       <p className="desc">{description}</p>
       <div className="meta">
